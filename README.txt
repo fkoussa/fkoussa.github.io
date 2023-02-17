bc3c80183852c4de6e984ae803932623fc999aea
fkoussa <firaskoussa@gmail.com>
2023-02-17 05:07:35 +0400

    ready to deploy

--- README.txt
+++ README.txt
@@ -1,3 +1,3 @@
-Github Repo:
+Github Repo: https://github.com/fkoussa/school_ums_express
 Github Page (Front-End Code):
 Heroku (Back-End): 
--- index.js
+++ index.js
@@ -35,4 +35,4 @@ app.get("/", async (req, res) => {
 });
 
 // Start the server on port 3000
-app.listen(3000, () => console.log("Listening on port 3000!"));
+app.listen(process.env.PORT || 3000, () => console.log("Server is running..."));
